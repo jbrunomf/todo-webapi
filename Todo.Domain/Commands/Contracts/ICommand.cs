be9225dad5d7a1944aabfbc3d94d304518a81f58
Joao Bruno <jbrunomf@outlook.com>
2022-08-29 18:51:30 -0400

    rever to flunt 1.0.5. adicionando CreateTodoCommand

--- Todo.Domain/Commands/Contracts/ICommand.cs
+++ Todo.Domain/Commands/Contracts/ICommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Flunt.Validations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace Todo.Domain.Commands.Contracts
 {
-    public interface ICommand
+    public interface ICommand : IValidatable
     {
-        bool Validate();
+       
     }
 }
