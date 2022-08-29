ef48dd05b1a843d8047d0a307f26547ef6465d40
Joao Bruno <jbrunomf@outlook.com>
2022-08-29 19:47:23 -0400

    add: Command par amarcar um Todo como concluído.

---
+++ Todo.Domain/Commands/MarkTodoAsDoneCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class MarkTodoAsDoneCommand : Notifiable, ICommand
+    {
+        public MarkTodoAsDoneCommand()
+        {
+
+        }
+
+        public MarkTodoAsDoneCommand(Guid id, string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string User { get; set; }
+
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .HasLen(User, 6, "User", "Usuário inválido.")
+                );
+        }
+    }
+}
