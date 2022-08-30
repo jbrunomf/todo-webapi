dd8b721fde1851dcac2da7525d7a1b5fb200302d
Joao Bruno <jbrunomf@outlook.com>
2022-08-29 21:15:23 -0400

    Adicionando ITodoRepository

---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Handlers.Contracts;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>
+    {
+        private readonly ITodo
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
