30d9a4c236df27a48a42dee33f4ef49d5df18fa4
Joao Bruno <jbrunomf@outlook.com>
2022-08-31 19:43:55 -0400

    add: DataContext

---
+++ Todo.Domain.Infra/Contexts/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Infra.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
