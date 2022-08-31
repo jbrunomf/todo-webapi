0cd63375540942826d8dc4dc82cf6ae49ae017b6
Joao Bruno <jbrunomf@outlook.com>
2022-08-30 21:19:45 -0400

    finalizando os testes

---
+++ Todo.Domain.Tests/EntityTests/TodoItemTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.EntityTests
+{
+    [TestClass]
+    public class TodoItemTests
+    {
+        private readonly TodoItem _validTodoItem = new TodoItem("Titulo do TODO", DateTime.Now, "João");
+        //private readonly TodoItem _invaliTodoItem = new TodoItem("", DateTime.Now, "João");
+
+        [TestMethod]
+        public void Dado_um_novo_todo_o_mesmo_nao_pode_ser_concluido()
+        {
+             
+            Assert.AreEqual(_validTodoItem.Done, false);
+        }
+    }
+}
---
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,29 @@
+﻿using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+        [TestMethod]
+        public void Dado_um_comando_invalido_deve_interromper_execucao()
+        {
+            var command = new CreateTodoCommand("", "", DateTime.Now);
+            var handler = new TodoHandler(new FakeTodoRepository());
+            var result = (GenericCommandResult)handler.Handle(command);
+            Assert.AreEqual(result.Success, false);
+        }
+
+
+        [TestMethod]
+        public void Dado_um_comando_valido_deve_criar_tarefa()
+        {
+            var command = new CreateTodoCommand("Titulo da Tarefa", "João", DateTime.Now);
+            var handler = new TodoHandler(new FakeTodoRepository());
+            var result = (GenericCommandResult)handler.Handle(command);
+            Assert.AreEqual(result.Success, true);
+        }
+    }
+}
